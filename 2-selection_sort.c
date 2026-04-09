6f147771bf2eb3019c3ab3bd71a9fdaa0cd2e800
Adamzou-lab <adampro@icloud.com>
2026-04-09 15:54:21 +0200

    feat: add selection_sort function

---
+++ 2-selection_sort.c
@@ -0,0 +1,29 @@
+#include "sort.h"
+
+/**
+ * selection_sort - Sorts an array of integers using selection sort
+ * @array: Array to sort
+ * @size: Size of the array
+ */
+void selection_sort(int *array, size_t size)
+{
+	size_t i, j, min_idx;
+	int tmp;
+
+	for (i = 0; i < size - 1; i++)
+	{
+		min_idx = i;
+		for (j = i + 1; j < size; j++)
+		{
+			if (array[j] < array[min_idx])
+				min_idx = j;
+		}
+		if (min_idx != i)
+		{
+			tmp = array[i];
+			array[i] = array[min_idx];
+			array[min_idx] = tmp;
+			print_array(array, size);
+		}
+	}
+}
