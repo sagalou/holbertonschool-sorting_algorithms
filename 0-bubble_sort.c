ce15b777aa7bd69e0864daa05879e2e5921c81ed
Adamzou-lab <adampro@icloud.com>
2026-04-09 14:56:54 +0200

    feat: add bubble_sort function + proto

---
+++ 0-bubble_sort.c
@@ -0,0 +1,27 @@
+#include "sort.h"
+
+void bubble_sort(int *array, size_t size)
+{
+	size_t i, j;
+	int tmp;
+
+	if (array == NULL || size < 2)
+		return;
+	i = 0;
+	while (i < size - 1)
+	{
+		j = 0;
+		while (j < size - 1 - i)
+		{
+			if (array[j] > array[j + 1])
+			{
+				tmp = array[j];
+				array[j] = array[j + 1];
+				array[j + 1] = tmp;
+				print_array(array, size);
+			}
+			j++;
+		}
+		i++;
+	}
+}
