75bf58e29074da14cf8d4fc9e381762539d84744
Adamzou-lab <adampro@icloud.com>
2026-04-09 15:03:36 +0200

    feat: add betty description

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,5 +1,12 @@
 #include "sort.h"
-
+/**
+ * bubble_sort - Sorts an array of integers in ascending order
+ * using the Bubble sort algorithm.
+ * @array: An array of integers to sort.
+ * @size: The size of the array.
+ *
+ * Description: Prints the array after each swap.
+ */
 void bubble_sort(int *array, size_t size)
 {
 	size_t i, j;
